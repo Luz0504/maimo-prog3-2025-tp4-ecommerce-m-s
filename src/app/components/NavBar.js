779b7c03a7b11a71bf73cf6ab1e240649ba8b8e7
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-08-21 19:35:50 -0300

    Adding Navbar

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <h1>Pagina de testeo</h1>
+        <ul className='flex'>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
