8f6516131505e76267064bfd482dc43830c417b8
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-08-27 20:08:51 -0300

    improving navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import Image from 'next/image'
 
 const NavBar = () => {
   return (
-    <nav>
-        <h1>Pagina de testeo</h1>
-        <ul className='flex'>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+    <nav className=' bg-fuchsia-900'>
+        <h1 className='p-5 text-center text-4xl font-bold'>GAMERZ STORE</h1>
+        <div className='flex justify-between items-center px-10 bg-blue-500 py-2 rounded-t-4xl'>
+            <ul className='flex gap-3'>
+                <li>CATEGORIAS</li>
+                <li>COLECCIONES</li>
+                <li>GIFT CARDS</li>
+                <li>AYUDA</li>
+                <li>DEVOLUCIONES</li>
+
+            </ul>
+            <ul className='flex gap-3'>
+                <li><Image
+                src={'/assets/acc.png'}
+                alt={"Your acc"}
+                height={30}
+                width={30}
+                /></li>
+                <li><Image
+                src={'/assets/cart.png'}
+                alt={"Your cart"}
+                height={30}
+                width={30}
+                /></li>
+
+            </ul>
+        </div>
     </nav>
   )
 }
