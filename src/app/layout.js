ac49450b61f6df53ddf2119d9a9fe56474e6dbdd
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-08-28 12:57:14 -0300

    added searchbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar/>
+        <SearchBar/>
         {children}
       </body>
     </html>
