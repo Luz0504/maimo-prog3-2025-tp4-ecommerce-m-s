f42016a64a7df3f28ecfdc738c90ba3c9f1e6dcb
Luz <mlsueldo59@gmail.com>
2025-09-03 18:11:42 -0300

    Agregado Banner

---
+++ src/app/components/Banner.js
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div className='flex border-2 h-[200px] border-black bg-fuchsia-500'>
+        <p>
+            Pruebaa
+        </p>
+    </div>
+  )
+}
+
+export default Banner
