27ea719901f2e302319525ba4a57ec3f4f08e954
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-09-03 17:24:28 -0300

    starting burgermenu

---
+++ src/app/components/BurgerMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BurgerMenu = () => {
+  return (
+    <p>Boy what the hell</p>
+  )
+}
+
+export default BurgerMenu
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import BurgerMenu from "./components/BurgerMenu.js";
 
 export default function Home() {
   //uh oh. HELPPPPPPPPPPPPPPPPPPPPPP
   return (
     <>
-      
+    <BurgerMenu/>
     </>
   );
 }
