fd5baf6b42fa59c7e5687e70f74ee849c54d058c
Luz <mlsueldo59@gmail.com>
2025-09-24 15:12:59 -0300

    Mejorado el menú hamburguesa y agregados links

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -1,30 +1,24 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
-const ProductCard = ({name, price, img}) => {
-
+const ProductCard = ({ id, name, price, img }) => {
   return (
     <div className="flex flex-col p-4 bg-white rounded-2xl gap-2 border-black border-[2px] text-black transition-transform duration-300 ease-in-out hover:scale-105">
-        <div className='flex justify-center'>
-            <Image
-            src={'/assets/sunny.jpg'}
-            height={500}
-            width={150}
-            alt={name}
-            />
-        </div>
-        <div className="h-15">
-            <h1 className="line-clamp-1">
-                {name}
-            </h1>
-            <p>
-                {price}
-            </p>
-        </div>
-        <button className="bg-blue-400 rounded-3xl border-black border-[2px] p-1 duration-100 ease-in-out hover:bg-blue-500">
-            Ver más
-        </button>
+      <div className="flex justify-center">
+        <Image src={"/assets/sunny.jpg"} height={500} width={150} alt={name} />
+      </div>
+      <div className="h-15">
+        <h1 className="line-clamp-1">{name}</h1>
+        <p>{price}</p>
+      </div>
+      <Link
+        href={`/product/${id}`}
+        className="bg-blue-400 rounded-3xl border-black border-[2px] p-1 duration-100 ease-in-out hover:bg-blue-500 hover:cursor-pointer text-center"
+      >
+        <button>Ver más</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
