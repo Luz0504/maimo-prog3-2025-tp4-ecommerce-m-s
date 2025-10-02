02f585c1588518c2b1f5201fe7d6afba0c9fd81e
Luz <mlsueldo59@gmail.com>
2025-10-02 20:55:56 -0300

    editando banner

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,16 +1,14 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Banner = () => {
-  return (
-    <div className='w-full mb-7 border-2 h-[200px] rounded-3xl border-black bg-fuchsia-500 flex justify-items-center overflow-hidden'>
-           <Image
-            src={'/assets/sunny.jpg'}
-            width={1000}
-            height={50}
-            alt='snuuy'
-            />
-    </div>
-  )
-}
 
-export default Banner
+  const bannerImgs = ['DeltaBanner.png', 'OffBanner.png', 'IbBanner.png', 'OmoriBanner.png']
+
+  return (
+    <div className="w-full mb-7 border-2 h-[200px] rounded-3xl border-black bg-fuchsia-500 flex justify-items-center overflow-hidden">
+      <Image src={"/assets/sunny.jpg"} width={1000} height={50} alt="snuuy" />
+    </div>
+  );
+};
+
+export default Banner;
