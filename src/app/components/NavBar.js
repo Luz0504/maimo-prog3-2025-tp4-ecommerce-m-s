c613147b12f170c1b1ad6c0a9622d3d71404651c
Luz <mlsueldo59@gmail.com>
2025-10-02 19:22:45 -0300

    agregado pag. principal al NavBar y arreglado ProductContainer

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <nav className=' bg-fuchsia-900'>
-        <h1 className='p-5 text-center text-4xl font-bold'>GAMERZ STORE</h1>
+        <Link href={'/'}><h1 className='p-5 text-center text-4xl font-bold hover:text-white'>GAMERZ STORE</h1></Link>
         <div className='flex justify-between items-center px-10 bg-blue-500 py-2 rounded-t-4xl'>
             <ul className='flex gap-3 text-[0.75em]'>
                 <li>CATEGORIAS</li>
