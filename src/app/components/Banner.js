f972ae7e0f71561e11aec30d0d97ed557234fa7e
Luz <mlsueldo59@gmail.com>
2025-10-09 20:33:42 -0300

    banner actualizado

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,14 +1,11 @@
-import Image from "next/image";
+import SimpleSlider from './SimpleSlider'
 
 const Banner = () => {
-
-  const bannerImgs = ['DeltaBanner.png', 'OffBanner.png', 'IbBanner.png', 'OmoriBanner.png']
-
   return (
-    <div className="w-full mb-7 border-2 h-[200px] rounded-3xl border-black bg-fuchsia-500 flex justify-items-center overflow-hidden">
-      <Image src={"/assets/sunny.jpg"} width={1000} height={50} alt="snuuy" />
+    <div className='w-220 border-black rounded-3xl overflow-clip'>
+        <SimpleSlider />
     </div>
-  );
-};
+  )
+}
 
-export default Banner;
+export default Banner
