14759913f88c6043908871e9fddedce9cbdd8ada
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-10-09 19:20:12 -0300

    fixing small pet peeve with quitarCart

--- src/app/components/QuitarCart.js
+++ src/app/components/QuitarCart.js
@@ -6,7 +6,7 @@ const QuitarCart = ({ id }) => {
   return (
     <div
       onClick={() => removeFromCart(id)}
-      className="bg-fuchsia-700 rounded-4xl border-black border-[2px] text-white px-2 py-2.5 truncate w-fit hover: overflow-visible duration-200 ease-in cursor-pointer hover:bg-fuchsia-900"
+      className="bg-fuchsia-700 rounded-4xl border-black border-[2px] text-white px-2 py-2.5 truncate w-full hover: overflow-visible duration-200 ease-in cursor-pointer hover:bg-fuchsia-900"
     >
       <p className="flex justify-center">Quitar del carrito</p>
     </div>
