53606a6543821fa216e81a987a575048ac0556cc
Luz <mlsueldo59@gmail.com>
2025-10-12 17:56:40 -0300

    Arreglado el Banner

--- src/app/components/SimpleSlider.js
+++ src/app/components/SimpleSlider.js
@@ -5,54 +5,56 @@ import Slider from "react-slick";
 
 export default function SimpleSlider() {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     speed: 200,
     autoplaySpeed: 5000,
-    cssEase: "linear"
+    cssEase: "linear",
   };
 
   return (
-    <Slider {...settings}>
-      <div>
-        <Image
-          src={"/assets/OffBanner.png"}
-          height={400}
-          width={800}
-          alt="Off"
-          className="w-full object-cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={"/assets/IbBanner.png"}
-          height={400}
-          width={800}
-          alt="Ib"
-          className="w-full object-cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={"/assets/OmoriBanner.png"}
-          height={400}
-          width={800}
-          alt="Omori"
-          className="w-full object-cover"
-        />
-      </div>
-      <div>
-        <Image
-          src={"/assets/DeltaBanner.png"}
-          height={400}
-          width={800}
-          alt="Deltarune"
-          className="w-full object-cover"
-        />
-      </div>
-    </Slider>
+    <div className="h-[230px]">
+      <Slider {...settings}>
+        <div>
+          <Image
+            src={"/assets/OffBanner.png"}
+            height={400}
+            width={800}
+            alt="Off"
+            className="w-full object-cover"
+          />
+        </div>
+        <div>
+          <Image
+            src={"/assets/IbBanner.png"}
+            height={400}
+            width={800}
+            alt="Ib"
+            className="w-full object-cover"
+          />
+        </div>
+        <div>
+          <Image
+            src={"/assets/OmoriBanner.png"}
+            height={400}
+            width={800}
+            alt="Omori"
+            className="w-full object-cover"
+          />
+        </div>
+        <div>
+          <Image
+            src={"/assets/DeltaBanner.png"}
+            height={400}
+            width={800}
+            alt="Deltarune"
+            className="w-full object-cover"
+          />
+        </div>
+      </Slider>
+    </div>
   );
 }
